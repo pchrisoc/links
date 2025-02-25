057b4d01e345e139e4621e5a357e1e18e977cb05
pchrisoc <pchrisoc@berkeley.edu>
2025-02-24 20:29:20 -0800

    fix?

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+	build: {
+		outDir: 'public' // Change this if your framework requires a different directory
+	},
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
+	
 });
